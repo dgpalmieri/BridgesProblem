09b5ca20294f3bbdaf08a60dfe056bc2d7cd8530
Dylan Palmieri <dgpalmieri@gmail.com>
2020-09-28 15:09:35 -0800

    Add bridge function prototype

---
+++ bridge.hpp
@@ -0,0 +1,13 @@
+// bridge.hpp
+// Dylan Palmieri
+// 9/28/20
+// Header for the bridge function, used in CS411
+
+#ifndef BRIDGE_HPP
+#define BRIDGE_HPP
+
+#include<vector>
+
+int build(int w, int e, const vector<vector<int>> & bridges);
+
+#endif /* BRIDGE_HPP */
