466bc18c6beeb4b3047c74d4919cd5b418c05117
Dylan Palmieri <dgpalmieri@gmail.com>
2020-09-28 15:08:18 -0800

    Add template bridge function

---
+++ bridge.cpp
@@ -0,0 +1,13 @@
+// bridge.cpp
+// Dylan Palmieri
+// 9/28/20
+// Definition of bridge, CS411 Homework 2
+
+#include<vector>
+using std::vector;
+
+using Bridge = vector<int>;
+
+int build(int w, int e, const vector<Bridge> & bridges){
+    return 0;
+}
