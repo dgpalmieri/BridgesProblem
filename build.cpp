b411a4bddbbb1f6b1ef450a219a01e2a01ce282c
Dylan Palmieri <dgpalmieri@gmail.com>
2020-09-29 00:35:46 -0800

    Sort vectors by first item

--- build.cpp
+++ build.cpp
@@ -3,28 +3,42 @@
 // 9/28/20
 // Definition of bridge, CS411 Homework 2
 
+#include<algorithm>
+#include<tuple>
 #include<vector>
 using std::vector;
 
 using Bridge = vector<int>;
 
-int bridgeCount(const vector<Bridge> & bridges){
-    int score = 0;
-    for (const auto & i: bridges){
-        if (i[2] > score){
-            score = i[2];
-        }
-    } 
-    return score;
+bool bridgeIsValid( const vector<Bridge> & a, const Bridge & b ){
+    return true;
 }
 
-int build(int w, int e, const vector<Bridge> & bridges){
-    if (bridges.empty()) {
+int bridgeCount( const vector<Bridge> & bridges ){
+    int totalScore = 0;
+    vector<Bridge> scored_bridges = {};
+    for ( const auto & i: bridges ){
+        if (bridgeIsValid( scored_bridges, i ) && i[2] > totalScore){
+            totalScore += i[2];
+            scored_bridges.push_back(i);
+        }
+    } 
+    return totalScore;
+}
+
+// Sorting function implementation details inspired by Chris Hartman
+// https://github.com/AKQuaternion/Fall2019CS411DemoCode/blob/master/CS411Fall19HW2/build.cpp
+int build( int w, int e, const vector<Bridge> & bridges ){
+    if ( bridges.empty() ) {
         return 0;
     }
 
-    int score = 0;
-    score = bridgeCount(bridges);
+    vector<Bridge> sortedBridges(bridges);
 
-    return score;
+    std::sort( sortedBridges.begin(), sortedBridges.end(), 
+           [](const auto & a, const auto & b) {
+              return std::forward_as_tuple(a[0], a[1], a[2]) <
+                     std::forward_as_tuple(b[0], b[1], b[2]);
+                     });
+    return bridgeCount( bridges );
 }
